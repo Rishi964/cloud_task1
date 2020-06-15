fe1d97719a53075bdb6f3dfd8b2e003eb456a450
Rishabh Khandelwal <64452412+Rishi964@users.noreply.github.com>
2020-06-15 18:36:35 +0530

    last commit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ print "Hi! Welcome to this page.\n";
 print "IP address of this page is here:\n";
 print `ifconfig`;
 
-<img src='CF_domain' width='300' height='380'>
+<img src='CF_domain/image.png' width='300' height='380'>
 
 ?>
 </pre>
